33e2e4f92ad959dfe3e7c230e84a4f027b461132
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-17 11:46:24 +0200

    init

---
+++ src/main/java/pl/sda/covidvavapp/CovidVavAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.covidvavapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVavAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidVavAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/covidvavapp/CovidVavAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.covidvavapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVavAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
