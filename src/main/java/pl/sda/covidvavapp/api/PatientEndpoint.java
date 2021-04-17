6f08337c69799a7b9d1d70f09c6b048160e0769d
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-17 14:22:51 +0200

    patients endpoint

--- src/main/java/pl/sda/covidvavapp/api/PatientEndpoint.java
+++ src/main/java/pl/sda/covidvavapp/api/PatientEndpoint.java
@@ -1,41 +1,46 @@
 package pl.sda.covidvavapp.api;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.covidvavapp.api.model.NewPatient;
 import pl.sda.covidvavapp.api.model.Patient;
 import pl.sda.covidvavapp.api.model.UpdatePatient;
+import pl.sda.covidvavapp.service.PatientService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/patient")
+@AllArgsConstructor
 public class PatientEndpoint {
+
+    private PatientService patientService;
 
     @GetMapping
     public List<Patient> getAll() {
-        return null;
+        return patientService.getAllPatients();
     }
 
     @GetMapping("/{id}")
     public Patient getOne(@PathVariable Long id) {
-        return null;
+        return patientService.getPatient(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void registerPatient(@RequestBody NewPatient patient) {
-
+        patientService.registerPatient(patient);
     }
 
     @PutMapping
     public void updatePatient(@RequestBody UpdatePatient patient) {
-
+        patientService.updatePatient(patient);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePatient(@PathVariable Long id) {
-
+        patientService.deletePatient(id);
     }
 }
