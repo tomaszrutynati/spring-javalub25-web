bb3916f366506a63a1a12f12b59e2f83638f3294
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-18 14:16:13 +0200

    mvc - first dynamic page

--- src/main/java/pl/sda/covidvavapp/web/MainPageController.java
+++ src/main/java/pl/sda/covidvavapp/web/MainPageController.java
@@ -2,13 +2,24 @@ package pl.sda.covidvavapp.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
 
 @Controller
 public class MainPageController {
 
-    @GetMapping("/")
-    public String displayMainPage() {
+  /*  @GetMapping("/")
+    public String displayMainPage(final ModelMap modelMap) {
+        modelMap.addAttribute("currentDate", LocalDate.now());
         return "main";
+    }*/
+
+    @GetMapping("/")
+    public ModelAndView displayMainPage() {
+        ModelAndView mav = new ModelAndView("main");
+        mav.addObject("currentDate", LocalDate.now());
+        return mav;
     }
 
 }
