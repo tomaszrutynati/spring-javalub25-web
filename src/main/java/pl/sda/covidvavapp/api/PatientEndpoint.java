4ae88e2c9f2340892132174cf51f5f220f8bd1a6
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-04-18 11:05:44 +0200

    validation

--- src/main/java/pl/sda/covidvavapp/api/PatientEndpoint.java
+++ src/main/java/pl/sda/covidvavapp/api/PatientEndpoint.java
@@ -1,14 +1,20 @@
 package pl.sda.covidvavapp.api;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import pl.sda.covidvavapp.api.model.Errors;
 import pl.sda.covidvavapp.api.model.NewPatient;
 import pl.sda.covidvavapp.api.model.Patient;
 import pl.sda.covidvavapp.api.model.UpdatePatient;
 import pl.sda.covidvavapp.service.PatientService;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/patient")
@@ -29,8 +35,18 @@ public class PatientEndpoint {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void registerPatient(@RequestBody NewPatient patient) {
-        patientService.registerPatient(patient);
+    public ResponseEntity registerPatient(@Valid @RequestBody NewPatient patient, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errorsMsgs = bindingResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            Errors errors = Errors.builder().errors(errorsMsgs).build();
+
+            return ResponseEntity.status(400).body(errors);
+        } else {
+            patientService.registerPatient(patient);
+            return ResponseEntity.status(201).build();
+        }
     }
 
     @PutMapping
--- src/main/java/pl/sda/covidvavapp/api/model/NewPatient.java
+++ src/main/java/pl/sda/covidvavapp/api/model/NewPatient.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.pl.PESEL;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -12,8 +15,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewPatient {
+    @NotNull(message = "First name should not be null")
     private String firstName;
+    @NotNull(message = "Last name should not be null")
     private String lastName;
     private LocalDate birthDate;
+    @PESEL(message = "PESEL should have proper value")
     private String pesel;
+
+    @AssertTrue(message = "Birth date should be in past")
+    public boolean isBirthDateInPast() {
+        return birthDate.isBefore(LocalDate.now());
+    }
 }
